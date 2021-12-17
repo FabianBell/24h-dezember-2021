9d20b8eb25bafb619ab27d7ed27465277bac0b0c
Noxsense <noxsense@gmail.com>
2021-12-17 17:25:30 +0100

    Simple Frontend Overhaul and request creation.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,8 @@
  */
 
 import {LitElement, html, css} from 'lit';
-import { boostrapStyle } from './Bootstrap';
+import {boostrapStyle} from './Bootstrap';
+import {repeat} from 'lit/directives/repeat.js';
 
 /**
  * An example element.
@@ -15,22 +16,190 @@ import { boostrapStyle } from './Bootstrap';
  * @csspart button - The button
  */
 export class Main extends LitElement {
+  image = null;
+  imageBase64 = '';
+  modells = [];
+  webSocket = null;
+
+  modeloptions = ['GFPGRAN', 'ARCANA'];
+
   static get styles() {
     return boostrapStyle;
   }
 
   render() {
     return html`
-      <h1>Hier könnte Ihre Werbung stehen</h1>
-      <input class="form-control" type="file" id="formFile">
-      <button type="button" class="btn btn-primary">TEST</button>
+      <div style="padding: 1%;">
+        <h1>Hier k&ouml;nnte Ihre Werbung stehen</h1>
+
+        <div style="padding:7px;">
+          <div style="padding: 7px 0px;">
+            Upload your image:
+            <input
+              id="inputImage"
+              class="form-control"
+              type="file"
+              value="${this.image || ''}"
+              @change="${this._updateFile}"
+            />
+            <img
+              id="inputImage-preview"
+              src=""
+              style="width:100%; padding: 5%;"
+            />
+          </div>
+          <div style="padding: 7px 0px;">
+            Choose your modell.
+            <div>
+              <ol id="modells">
+                ${repeat(
+                  this.modells,
+                  (chosenModell) => html`<li>${chosenModell}</li>`
+                )}
+              </ol>
+              <button @click="${() => this._apply('GFPGRAN')}">GFPGRAN</button>
+              <button @click="${() => this._apply('ARCANA')}">ARCANA</button>
+              <button @click="${() => console.log('Nope')}">...</button>
+            </div>
+          </div>
+          <div style="padding: 7px 0px;">
+            <button
+              id="submit"
+              type="button"
+              class="btn btn-primary"
+              @click="${this._submit}"
+              style="width: 100%;"
+            >
+              Submit !
+            </button>
+          </div>
+          <div style="padding: 7px 0px;">
+            <img
+              id="output-preview"
+              src="https://huggingface.co/front/assets/huggingface_logo-noborder.svg"
+              style="width:100%; padding: 5%;"
+            />
+            <p id="message"></p>
+          </div>
+        </div>
+      </div>
     `;
   }
 
-  _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
-  }
+  _apply = function (modell) {
+    this.modells.push(modell);
+    console.log(this.modells);
+  };
+
+  _updateFile = function () {
+    // get latest file.
+    let fileInput = this.renderRoot.querySelector('#inputImage');
+
+    let file = fileInput.files[0];
+    console.log(file);
+
+    this.image = file;
+    console.log(this.image);
+
+    // preview file.
+    let filePreviewer = this.renderRoot.querySelector('#inputImage-preview');
+    let reader = this._getBase64(file, filePreviewer);
+
+    console.log(reader.result);
+  };
+
+  _getBase64 = function (file, imageComponent) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      this.imageBase64 = reader.result;
+
+      // preview.
+      imageComponent.src = this.imageBase64;
+    };
+    return reader;
+  };
+
+  _submit = function () {
+    let messageComponent = this.renderRoot.querySelector('#message');
+
+    // --- SEND REQUEST TO CONTROLLER. ---------------------------------------
+    if (!this.image) {
+      messageComponent.innerHTML = `
+        <span style="color: red; font-weight: bolder;">
+          Could not upload this image.
+        </span>`;
+      return;
+    }
+
+    let request = {
+      image: this.image.name,
+      extension: this.image.type,
+      modells: this.modells,
+      sessionkey: 'sessionkey',
+    };
+
+    console.log(request);
+
+    messageComponent.innerHTML = `
+        <span style="color: blue; font-weight: normal;">
+          Uploading ...
+        </span>`;
+
+    // console.log(this.imageBase64);
+
+    let outputImageComponent = this.renderRoot.querySelector('#output-preview');
+    // outputImageComponent.src = this.image;
+    this._getBase64(this.image, outputImageComponent);
+
+    this._foo();
+
+    if (this.webSocket) {
+      this.webSocket.send("Hello websocket" + JSON.stringify(request));
+    }
+  };
+
+  _foo = function () {
+    debugger;
+    if (window.WebSocket === undefined) {
+      console.log('sockets not supported');
+    } else {
+      //
+      window.addEventListener('load', this.onLoad, false);
+    }
+  };
+
+  onLoad = function () {
+    let port = '7777';
+    let wsUri = 'ws://127.0.0.1:' + port;
+    this.webSocket = new WebSocket(wsUri);
+    this.webSocket.onopen = function (evt) {
+      this.onOpen(evt);
+    };
+    this.webSocket.onmessage = function (evt) {
+      this.onMessage(evt);
+    };
+    this.webSocket.onerror = function (evt) {
+      this.onError(evt);
+    };
+  };
+
+  onOpen = function (evt) {
+    console.log('Open', evt);
+  };
+
+  onMessage = function (evt) {
+    console.log('Message', evt);
+  };
+
+  onClose = function (evt) {
+    console.log('Close', evt);
+  };
+
+  // _onClick() {
+  //   this.count++;
+  //   this.dispatchEvent(new CustomEvent('count-changed'));
+  // }
 }
 
 window.customElements.define('service-main', Main);
